0fa7746b878083000f169cb3a9dc4e4909e5ec11
MahdiPU <75437793+MahdiPU@users.noreply.github.com>
2022-04-13 17:17:53 +0300

    create mongoosse task schema

--- dist/models/task.js
+++ dist/models/task.js
@@ -1,2 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const TaskSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: [true, 'must provide name'],
+        trim: true,
+        maxlength: [20, "name can not be more than 20 characters"],
+    },
+    completed: { type: Boolean, default: false },
+});
+exports.default = TaskSchema;
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -5,3 +5,14 @@ interface Task {
   completed: boolean;
 }
 
+const TaskSchema = new Schema<Task>({
+    name: {
+      type: String,
+      required: [true, 'must provide name'],
+      trim: true,
+      maxlength: [20, "name can not be more than 20 characters"],
+    },
+    completed: { type: Boolean, default: false },
+  });
+
+export default TaskSchema
