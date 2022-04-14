d6fa36191fbe98610717fe0e02468a31df726e67
MahdiPU <75437793+MahdiPU@users.noreply.github.com>
2022-04-14 10:56:05 +0300

    connect db

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
 import express, { Request, Response, NextFunction } from "express";
-import { json } from 'body-parser'
-
-
+import { json, urlencoded } from 'body-parser'
+import {connectDB} from './services/connect' 
+import tasks from './routes/tasks'
+import dotenv from 'dotenv'
+dotenv.config();
 const app = express()
-
-app.listen(3000)
+app.use(json())
+app.use('/',tasks)
+const start = async () => {
+    try {
+      await connectDB(process.env.MONGO_URI as string);
+      app.listen(3000, () =>
+        console.log(`Server is listening on port 3000...`)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  start();
